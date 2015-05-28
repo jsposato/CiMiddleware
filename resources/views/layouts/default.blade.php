0417f924de5d5d4b53ba0a57fac7d9bb60c0eca3
John Sposato <jsposato@mobiquityinc.com>
2015-05-28 09:41:37 -0400

    Fixed title

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <title>App Name - @yield('title')</title>
+    <title>@yield('title')</title>
 
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
